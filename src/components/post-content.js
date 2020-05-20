e7984d39a772d32119fc5cd14231e05c8bd8eebc
Anouchka MINKOUE OBAME <informaticaux@gmail.com>
2020-05-20 14:15:22 +0200

    add

---
+++ src/components/post-content.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class PostContent extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Contenu d'un post</h1>
+            </div>
+        )
+    }
+}
+
+//export default PostContent;
---
+++ src/components/post-list-item.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class PostListItem extends Component {
+    render() {
+        return (
+            <div>
+                <p>Element de la liste</p>
+            </div>
+        )
+    }
+}
